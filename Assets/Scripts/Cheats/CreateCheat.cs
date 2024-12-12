be669fe4e12032ebe1a50e95662e1db6ccdde1e8
Dao Trong Nghia <105330547+Nghiadao1@users.noreply.github.com>
2024-12-12 15:52:43 +0700

    fix cheat

--- Assets/Scripts/Cheats/CreateCheat.cs
+++ Assets/Scripts/Cheats/CreateCheat.cs
@@ -10,8 +10,7 @@ public class CreateCheat : GenericSingleton<CreateCheat>
     // Update is called once per frame
     void Update()
     {
-        //if ckick "c" key 5 times open popup popupLoader
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             popupLoader.ShowPopup();
         }
